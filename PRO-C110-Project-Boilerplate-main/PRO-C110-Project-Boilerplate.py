51f68d949dcdf5ddc24e5db4a0670a2daa087b3b
Felizvirusja <88406284+Felizvirusja@users.noreply.github.com>
2022-08-09 17:10:08 -0500

    Add files via upload

---
+++ PRO-C110-Project-Boilerplate-main/PRO-C110-Project-Boilerplate.py
@@ -0,0 +1,34 @@
+import cv2
+import cv2
+import numpy as np
+import tensorflow as tf
+
+model = tf.keras.models.load_model('keras_model.h5')
+
+video = cv2.VideoCapture(0)
+
+while True:
+
+	status , frame = video.read()
+	if status:
+
+		frame = cv2.flip(frame , 1)
+		img = cv2.resize(frame,(224,224))
+
+		test_image = np.array(img, dtype=np.float32)
+		test_image = np.expand_dims(test_image, axis=0)
+
+		normalised_image = test_image/255.0	
+	          
+		prediction = model.predict(normalised_image) 
+		
+		cv2.imshow('feed' , frame)
+
+		key = cv2.waitKey(1)
+		
+		if key == 32:
+			break
+
+video.release()
+
+cv2.destroyAllWindows()
